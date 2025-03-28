b52090fafdc4ec6ac7a259e4cc9afcb78af0427a
Gremiao <gremiao@gmail.com>
2025-03-28 18:20:26 -0300

    Adicionado melhoria ao Deserializador.

--- src/main/java/br/com/jatai/adapter/MessageAdapter.java
+++ src/main/java/br/com/jatai/adapter/MessageAdapter.java
@@ -3,7 +3,6 @@ package br.com.jatai.adapter;
 import br.com.jatai.model.CorrelationId;
 import br.com.jatai.model.Message;
 import com.google.gson.*;
-import org.apache.kafka.common.protocol.types.Field;
 
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
@@ -16,24 +15,19 @@ public class MessageAdapter implements JsonSerializer<Message>, JsonDeserializer
         obj.addProperty("type", message.getPayload().getClass().getName());
         obj.add("payload", context.serialize(message.getPayload()));
         obj.add("correlationId", context.serialize(message.getId()));
-        obj.add("comments", context.serialize(message.getComments()));
-        obj.add("date", context.serialize(message.getDateTime()));
+        obj.addProperty("comments", message.getComments());
+        obj.add("dateTime", context.serialize(message.getDateTime()));
         return obj;
     }
 
     @Override
     public Message deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
         var obj = jsonElement.getAsJsonObject();
-        var payloadType = obj.get("type").getAsString();
-        var correlationId = (CorrelationId) context.deserialize(obj.get("correlationId"), CorrelationId.class);
-        var comments = (String) context.deserialize(obj.get("comments"), Field.Str.class);
-        var dateTime = (LocalDateTime) context.deserialize(obj.get("dateTime"), Field.Str.class);
+        CorrelationId correlationId = context.deserialize(obj.get("correlationId"), CorrelationId.class);
+        String payload = new Gson().toJson(obj.get("payload"));
+        String comments = obj.get("comments").getAsString();
+        LocalDateTime dateTime = context.deserialize(obj.get("dateTime"), LocalDateTime.class);
 
-        try {
-            var payload = context.deserialize(obj.get("payload"), Class.forName(payloadType));
-            return new Message(correlationId, payload, comments, dateTime);
-        } catch (ClassNotFoundException e) {
-            throw new JsonParseException(e);
-        }
+        return new Message<>(correlationId, payload, comments, dateTime);
     }
 }
--- src/main/java/br/com/jatai/consumer/GsonDeserializer.java
+++ src/main/java/br/com/jatai/consumer/GsonDeserializer.java
@@ -4,12 +4,22 @@ import br.com.jatai.adapter.MessageAdapter;
 import br.com.jatai.model.Message;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import org.apache.kafka.common.serialization.Deserializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class GsonDeserializer implements Deserializer<Message> {
 
-    private final Gson gson = new GsonBuilder().registerTypeAdapter(Message.class, new MessageAdapter()).create();
+    private final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(Message.class, new MessageAdapter())
+            .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, type, context) -> {
+                String dateTimeString = json.getAsString();
+                return LocalDateTime.parse(dateTimeString, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            })
+            .create();
 
     @Override
     public Message deserialize(String s, byte[] bytes) {
